932c79089ceff61b418508f0cee645ff0960ebf9
Michael Ruppe <michael.ruppe@live.com.au>
2020-02-25 20:17:59 +1100

    update data-packaging : add Int, UInt

--- data-packaging.cpp
+++ data-packaging.cpp
@@ -12,3 +12,30 @@ float unPackFloat(uint8_t buf[sizeof(float)], int index) {
   memcpy(&temp, buf + index, sizeof(float));
   return temp;
 }
+
+// Pack the binary data for a int variable into a buffer for a bitstream
+void packInt(int var, uint8_t buf[sizeof(float)], uint8_t index) {
+  memcpy(buf+index, &var, sizeof(float));
+
+}
+
+// UnPack the binary data for a int variable from a buffer
+int unPackInt(uint8_t buf[sizeof(int)], int index) {
+  int temp;
+  memcpy(&temp, buf + index, sizeof(int));
+  return temp;
+}
+
+
+// Pack the binary data for a unsigned int variable into a buffer for a bitstream
+void packUint(int var, uint8_t buf[sizeof(unsigned int)], uint8_t index) {
+  memcpy(buf+index, &var, sizeof(unsigned int));
+
+}
+
+// UnPack the binary data for a unsigned int variable from a buffer
+unsigned int unPackUint(uint8_t buf[sizeof(unsigned int)], int index) {
+  unsigned int temp;
+  memcpy(&temp, buf + index, sizeof(unsigned int));
+  return temp;
+}
--- data-packaging.h
+++ data-packaging.h
@@ -1,14 +1,15 @@
 #ifndef DATA_PACKAGING_H
 #define DATA_PACKAGING_H
 
-#include <stdint.h>
 #include "Arduino.h"
+#include <stdint.h>
 
 // Pack the binary data for a float variable into a buffer for a bitstream
 void packFloat(float var, uint8_t buf[sizeof(float)], uint8_t index);
-
-// UnPack the binary data for a float variable from a buffer
+void packInt(int var, uint8_t buf[sizeof(float)], uint8_t index);
+void packUint(int var, uint8_t buf[sizeof(unsigned int)], uint8_t index);
 float unPackFloat(uint8_t buf[sizeof(float)], int index);
-
+int unPackInt(uint8_t buf[sizeof(int)], int index);
+unsigned int unPackUint(uint8_t buf[sizeof(unsigned int)], int index);
 
 #endif
